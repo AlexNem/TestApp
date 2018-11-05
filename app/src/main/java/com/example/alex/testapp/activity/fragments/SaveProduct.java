5f5c838cb493687fabf0c7583fb42deacb852c88
Oleksandr Nemyrovskyi <AlexNem@users.noreply.github.com>
2018-11-05 23:34:54 +0200

    Add RecyclerView for product item

--- app/src/main/java/com/example/alex/testapp/activity/fragments/SaveProduct.java
+++ app/src/main/java/com/example/alex/testapp/activity/fragments/SaveProduct.java
@@ -4,20 +4,54 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.alex.testapp.R;
+import com.example.alex.testapp.recycler_view.ProductFragment;
+import com.example.alex.testapp.recycler_view.ProductRecViewAdapter;
+import com.example.alex.testapp.recycler_view.dummy.DummyContent;
 
-public class SaveProduct extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
+
+public class SaveProduct extends Fragment implements ProductFragment.OnListFragmentInteractionListener {
 
     private View view;
+    private ProductFragment.OnListFragmentInteractionListener listener;
+    private List<DummyContent.DummyItem> dummyItemList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_save_product, container, false);
         return view;
+
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        initRecycler();
+    }
+
+    private void initRecycler(){
+        RecyclerView recyclerView = view.findViewById(R.id.rec_list);
+        GridLayoutManager manager = new GridLayoutManager(getContext(),2);
+        recyclerView.setLayoutManager(manager);
+        ProductRecViewAdapter recViewAdapter = new ProductRecViewAdapter(DummyContent.ITEMS ,listener);
+        recyclerView.setAdapter(recViewAdapter);
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
     }
 }
---
+++ app/src/main/java/com/example/alex/testapp/recycler_view/ProductRecViewAdapter.java
@@ -0,0 +1,79 @@
+package com.example.alex.testapp.recycler_view;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.alex.testapp.R;
+import com.example.alex.testapp.recycler_view.ProductFragment.OnListFragmentInteractionListener;
+import com.example.alex.testapp.recycler_view.dummy.DummyContent.DummyItem;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link OnListFragmentInteractionListener}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class ProductRecViewAdapter extends RecyclerView.Adapter<ProductRecViewAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+    private final OnListFragmentInteractionListener mListener;
+
+    public ProductRecViewAdapter(List<DummyItem> items, OnListFragmentInteractionListener listener) {
+        mValues = items;
+        mListener = listener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_product, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.productImage.setText(mValues.get(position).details);
+        holder.productName.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (null != mListener) {
+                    // Notify the active callbacks interface (the activity, if the
+                    // fragment is attached to one) that an item has been selected.
+                    mListener.onListFragmentInteraction(holder.mItem);
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        private final View mView;
+        private final TextView productImage;
+        private final TextView productName;
+        private DummyItem mItem;
+
+        private ViewHolder(View view) {
+            super(view);
+            mView = view;
+            productImage = view.findViewById(R.id.image_product);
+            productName = view.findViewById(R.id.product_name);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + productName.getText() + "'";
+        }
+    }
+}
